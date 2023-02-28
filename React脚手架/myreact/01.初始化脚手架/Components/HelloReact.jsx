47c7d2ed32094ff686401874a576533979e4c6a6
zzy <1246486788@qq.com>
2023-02-28 14:43:56 +0800

    了解React的生命周期

--- React脚手架/myreact/01.初始化脚手架/Components/HelloReact.jsx
+++ React脚手架/myreact/01.初始化脚手架/Components/HelloReact.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 class HelloReact extends React.Component {
     constructor() {
-        console.log('HR-constructor')
         super();
         this.state = {
             name: 'react'
@@ -10,24 +9,11 @@ class HelloReact extends React.Component {
     }
 
     render() {
-        console.log('HR-render')
         return (
             <div>
                 <h2>Hello React!</h2>
             </div>
         )
-    }
-
-    componentDidMount() {
-        console.log('HR-componentDidMount')
-    }
-
-    componentDidUpdate() {
-        console.log('HR-componentDidUpdate')
-    }
-
-    componentWillUnmount() {
-        console.log('HR-componentWillUnmount')
     }
 }
 
