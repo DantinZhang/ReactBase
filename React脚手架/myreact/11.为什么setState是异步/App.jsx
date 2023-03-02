2e0f4e5cf9cdfdde13705ecf320a062a280a22aa
zzy <1246486788@qq.com>
2023-03-02 10:25:10 +0800

    为什么setState设置为异步（git结构调整）

---
+++ React脚手架/myreact/11.为什么setState是异步/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Son from './Son';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Son habit='学习'/>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ React脚手架/myreact/11.为什么setState是异步/Son.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import GrandSon from './GrandSon';
+
+export class Son extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'zzy',
+      age: 18,
+    }
+  }
+
+  changeName() {
+    // this.setState({
+    //   name: this.state.name + 'ht'
+    // })
+    // this.setState({
+    //   name: this.state.name + 'ht'
+    // })
+    // this.setState({
+    //   name: this.state.name + 'ht'
+    // })
+    this.setState((state) => {
+      return {
+        name: state.name + 'ht'
+      }
+    })
+    this.setState((state) => {
+      return {
+        name: state.name + 'ht'
+      }
+    })
+    this.setState((state) => {
+      return {
+        name: state.name + 'ht'
+      }
+    })
+  }
+
+  changeAge() {
+    this.setState({
+      age: 100
+    })
+    //一大坨代码，要执行一年
+  }
+  render() {
+    console.log('render执行',this.state.age)
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <button onClick={() => this.changeName()}>点击修改名字</button>
+        <button onClick={(() => this.changeAge())}>点击修改年龄</button>
+        <GrandSon age={this.state.age}/>
+      </div>
+
+    )
+  }
+}
+
+export default Son
