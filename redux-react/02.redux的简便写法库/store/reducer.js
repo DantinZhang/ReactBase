a747612b04354c0ab95b77d13d29c931d34f4072
zzy <1246486788@qq.com>
2023-03-06 14:11:29 +0800

    react-redux库的使用，可以不订阅就可以监听数据

---
+++ redux-react/02.redux的简便写法库/store/reducer.js
@@ -0,0 +1,17 @@
+import * as actionTypes from './constants'
+
+let initialState = {
+    counter: 1
+}
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        case actionTypes.CHANGECOUNTER:
+            return {...state, counter:state.counter + action.num};
+        case actionTypes.SUBTRACT:
+            return {...state, counter:state.counter + action.dj}
+        default:
+            return state;
+    }
+}
+
+export default reducer;
