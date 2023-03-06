63a44ecd3b79544d93bce48e3421bfc88d123eb6
zzy <1246486788@qq.com>
2023-03-06 21:43:00 +0800

    redux的devtools如何在开发环境下使用

--- redux-react/src/store/index.js
+++ redux-react/src/store/index.js
@@ -1,7 +1,9 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
 
-let store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
