7c5c760609925ecb708f8e8863a41e51a82115f8
zzy <1246486788@qq.com>
2023-03-07 19:14:57 +0800

    react toolkit是如何在redux中处理异步请求数据操作的

--- redux-toolkit/src/pages/Home.jsx
+++ redux-toolkit/src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { reqNewsList } from '../store/modules/home';
 
 export class Home extends PureComponent {
+  componentDidMount() {
+    this.props.getNewsList();
+  }
+
   render() {
     const {newsList} = this.props;
     return (
       <div>
         <h2>Home</h2>
         <ul>
-          {newsList.map(item => {
-            return <li key={item.id}>{item.title}</li>
+          {newsList.map((item,index) => {
+            return <li key={index}>{item.title}</li>
           })}
         </ul>
       </div>
@@ -23,4 +28,12 @@ const mapStateToProps = (state) => {
     newsList: state.home.newsList
   }
 }
-export default connect(mapStateToProps)(Home)
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getNewsList() {
+      dispatch(reqNewsList())
+    }
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
--- redux-toolkit/src/store/modules/home.js
+++ redux-toolkit/src/store/modules/home.js
@@ -1,4 +1,11 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+//异步操作的处理
+export const reqNewsList = createAsyncThunk('reqData', async () => {
+    const result = await axios.get('https://ku.qingnian8.com/dataApi/news/newslist.php')
+    return result.data;
+})
 
 const homeSlice = createSlice({
     name: 'home',
@@ -9,9 +16,25 @@ const homeSlice = createSlice({
     reducers: {
         getNewsList(state, {payload}) {
             console.log(payload);
+            state.newsList = payload;
         }
+    },
+
+    //监听异步的状态变化
+    extraReducers: {
+        [reqNewsList.pending](state, {payload}) {
+            console.log('异步处于pending状态');
+        },
+        [reqNewsList.fulfilled](state, {payload}) {
+            console.log(payload);
+            state.newsList = payload;
+        },
+        [reqNewsList.rejected](state, {payload}) {
+            console.log('异步处于rejected状态，有错误！');
+        },
     }
 })
 
 export const {getNewsList} = homeSlice.actions;
-export default homeSlice.reducer
+export default homeSlice.reducer;
+
