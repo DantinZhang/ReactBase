fa0896b6f9e7881ea4856f3b80ba617b900a7c33
zzy <1246486788@qq.com>
2023-03-08 15:00:34 +0800

    嵌套路由的使用（上）：声明式路由导航

---
+++ router/02.嵌套路由-声明式导航/pages/Column1.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Column1 extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h3>Column1</h3>
+      </div>
+    )
+  }
+}
+
+export default Column1
---
+++ router/src/pages/Column2.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Column2 extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h3>Column2</h3>
+      </div>
+    )
+  }
+}
+
+export default Column2
